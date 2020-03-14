711c771d3fae3e0f7b42ce935dce4e9a505380c9
g1xus <g1xus@yandex.ru>
2020-03-14 19:58:01 +0300

    remade header, made up offer block

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.header-toggle').click(function(){
+        $('.header-wrapper').toggleClass('header-wrapper_active')
+        $('.header-toggle__item').toggleClass('header-toggle__item_active')
+    })
+})
