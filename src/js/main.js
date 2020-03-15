768febf59fc89a88203b446f90175404630191be
g1xus <g1xus@yandex.ru>
2020-03-15 15:59:24 +0300

    made up and adaptated 'faq' block

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
         $('.header-wrapper').toggleClass('header-wrapper_active')
         $('.header-toggle__item').toggleClass('header-toggle__item_active')
     })
+    $('[data-toggle="tooltip"]').tooltip();
 })
